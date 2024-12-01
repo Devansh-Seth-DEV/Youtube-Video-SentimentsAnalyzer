bf7486c1ea7b15d033bf3f3b7a43ea72944f3984
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2024-12-02 03:38:11 +0530

    Delete api_secrets.py

--- api_secrets.py
+++
@@ -1,8 +0,0 @@
-API_KEY_ASSEMBLYAI: str = "2396e3cdd1b345549d7a9fdb0f27c9e8";
-HEADERS: dict           = {"authorization": API_KEY_ASSEMBLYAI};
-
-# URL's -----------------------------------------------------------------
-
-UPLOAD_URL: str         = "https://api.assemblyai.com/v2/upload";
-TRANSCRIPT_URL: str     = "https://api.assemblyai.com/v2/transcript";
-POLL_URL: str           = TRANSCRIPT_URL+"/";
