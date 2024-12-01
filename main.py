e34f387e37d49ee98cc6c6d2a1ff012f69df7e22
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2024-12-02 03:36:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,73 @@
 from json import load as JSON_LOAD
-from requests import get as REQ_GET
-from api_communications import FPATH, JSON, SENT, SITE_RESPONSE, AUDSaveTranscript
-from time import sleep as SLEEP_FOR_SEC
+from api_communications import *
+from api_communications import FPATH
 import yt_extractor as ytext
 import os
 
-def YTVIDSaveSentiments(ydl: ytext.YTDL, url: ytext.URL, data_dir: FPATH) -> FPATH:
-    video_info: ytext.VID_INFO  = ytext.YDLGetVideoInfo(ydl, url);
-    audio_url: ytext.URL        = ytext.YDLGetAudioURL(video_info);
-    
-    title: FPATH                = data_dir+"video";
-    thumbReq: SITE_RESPONSE     = REQ_GET(url = ytext.YDLGetVideoThumbnailURL(video_info));
 
-    with open(data_dir+"thumbnail.jpg", "wb") as fobj: fobj.write(thumbReq.content);
+class IVideoSentimentAnalyzer(ABC):
+    @abstractmethod
+    def saveSentiments(toPath: FPATH) -> FPATH: pass
 
-    json_data: JSON             = {
-        "audio_url": audio_url,
-        "sentiment_analysis": True
+    @abstractmethod
+    def analyzeSentiments(fromFile: FPATH) -> None: pass
+
+class YTVideoSentimentAnalyzer(IVideoSentimentAnalyzer):
+    def __init__(self, videoURL: ytext.URL, videoExtractor: ytext.IYTVideoExtractor, apiTranscriptManager: IApiTranscriptManager) -> None:
+        self.__videoURL = videoURL
+        self.__videoExtractor = videoExtractor
+        self.__apiTranscriptManager = apiTranscriptManager
+
+    def saveSentiments(self, toPath: FPATH) -> FPATH:
+        videoInfo = self.__videoExtractor.getVideoInfo(self.__videoURL)
+        
+        audioURL = ytext.YTAudioExtractor(videoInfo).getAudioURL()
+        infoExtractor = ytext.YTInfoExtractor(videoInfo)
+
+        title = infoExtractor.getTitle()
+        thumbReq: SITE_RESPONSE     = REQ_GET(url = infoExtractor.getThumbnailURL());
+
+        with open(toPath+"thumbnail.jpg", "wb") as fobj: fobj.write(thumbReq.content);
+
+        self.__apiTranscriptManager.saveTranscript(title, toPath);
+        return toPath + title + "_sentiments.json";
+
+
+    def analyzeSentiments(fromFile: str) -> None:
+        print("Analyzing Sentiments !\n");
+        with open(fromFile, "r") as fobj:
+            data    = JSON_LOAD(fobj);
+
+        sentiments: SENT    = {
+            "NEGATIVE"  : [],
+            "NEUTRAL"   : [],
+            "POSITIVE"  : [],
         };
 
-    AUDSaveTranscript(title, json_data);
-    return title + "_sentiments.json";
+        for res in data:
+            text: str   = res["text"];
+            sentiments[res["sentiment"]].append(text);
+
+        tot_neg: int    = len(sentiments["NEGATIVE"]);
+        tot_neu: int    = len(sentiments["NEUTRAL"]);
+        tot_pos: int    = len(sentiments["POSITIVE"]);
+
+        print("Positive sentiments  :", tot_pos);
+        print("Negative sentiments  :", tot_neg);
+        print("Neutral sentiments   :", tot_neu);
+
+        try:
+            pos_ratio: float  = tot_pos / (tot_pos + tot_neg);
+        except ZeroDivisionError:
+            pos_ratio: float    = 0;
+        print(f"Positive ratio       : {pos_ratio:.3f}");
+
+            
+
+# json_data: JSON             = {
+#     "audio_url": audioURL,
+#     "sentiment_analysis": True
+#     };
 
 def AnalyzeSentiments(file_path: FPATH) -> None:
     print("Analyzing Sentiments !\n");
